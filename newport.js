d6d45b73cd1f60aa4203b5a688ac153aa2fd1d74
ivelaz483 <89948666+ivelaz483@users.noreply.github.com>
2022-05-23 16:40:28 -0700

    Add files via upload

---
+++ newport.js
@@ -0,0 +1,82 @@
+window.onload = function() {
+    var canvas = document.getElementById("myCanvas0");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon0");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas1");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon1");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas2");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon2");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas3");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon3");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas4");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon4");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas5");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon5");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas6");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon6");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas7");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon7");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas8");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon8");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("myCanvas9");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("icon9");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+//Semester 2//
+var canvas = document.getElementById("Canvas0");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("picon0");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("Canvas1");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("picon1");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("Canvas2");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("picon2");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("Canvas3");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("picon3");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("Canvas4");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("picon4");
+   ctx.drawImage(img, 70, 80, 100, 100);
+
+   var canvas = document.getElementById("Canvas5");
+   var ctx = canvas.getContext("2d");
+   var img = document.getElementById("picon5");
+  ctx.drawImage(img, 70, 80, 50, 50);
+};
